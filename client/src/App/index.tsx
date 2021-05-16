1ce30e5393b5d7da454cb6e4e49f318a56509639
pradeep-selva <pradeepselvaraju@gmail.com>
2021-05-16 16:39:33 +0530

    minor changes

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,25 +1,10 @@
 import React from "react";
 import "./index.css";
+import theme from "./theme";
 import { Grommet } from "grommet";
 
-function App() {
-  return (
-    <Grommet className='App' plain>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </Grommet>
-  );
-}
+const App = () => {
+  return <Grommet className='App' theme={theme}></Grommet>;
+};
 
 export default App;
