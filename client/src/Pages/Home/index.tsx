1fb9f464e30e2e5b07377214c785d88c210078a6
pradeep-selva <pradeepselvaraju@gmail.com>
2021-05-21 00:44:48 +0530

    home sidebar configured

--- client/src/Pages/Home/index.tsx
+++ client/src/Pages/Home/index.tsx
@@ -1,8 +1,32 @@
+import { Box, Button, Nav, Sidebar } from "grommet";
+import { Logout } from "grommet-icons";
 import React, { Component } from "react";
 
 export class Home extends Component {
   render() {
-    return <h1>Home</h1>;
+    const tabs = ["About", "Projects", "Experience"];
+    return (
+      <Box direction={"row"}>
+        <Sidebar
+          style={{ height: "100vh", backgroundColor: "#005555" }}
+          background='brand'
+          round='small'
+          footer={<Button icon={<Logout />} hoverIndicator />}
+        >
+          <Nav gap='small'>
+            {tabs.map((tab) => (
+              <Button
+                key={tab}
+                size={"large"}
+                label={tab}
+                hoverIndicator={false}
+                style={{ border: 0 }}
+              />
+            ))}
+          </Nav>
+        </Sidebar>
+      </Box>
+    );
   }
 }
 
